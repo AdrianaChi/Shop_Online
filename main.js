cab1f93b673b42d81b0b0ed8761b652ca3ddd113
adrianach <adrianachirilescu@gmail.com>
2021-02-26 20:34:35 +0200

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+const currentLocation = location.href;
+const menuItem = document.querySelector('a');
+const menuLength = menuItem.length;
+
+for(let i = 0;i< menuLenght;i++)
+{
+    if(menuLength[i].href === currentLocation)
+    menuItem[i].className = 'active';
+};
+
+
+
+
+
+
+
